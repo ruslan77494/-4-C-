1a06f494b6f4b03837b34c983641b7eaac2f21c9
ruslan <rus.pro94@bk.ru>
2022-05-23 15:19:08 +0500

    028

---
+++ 028/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 28: Напишите программу, которая принимает
+ на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120
+*/
+
+bool IsNumberPOsitive(int number)
+{
+    return number > 0;
+}
+
+// cчитаем сумму чисел
+int GetProductFromoneToNumber(int number)
+{
+    int product = 1;
+
+    for(int i = 1; i <= number; i++)
+       product  *= i;
+
+    return product ;
+}
+
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+if(IsNumberPOsitive(number))
+{
+    int product = GetProductFromoneToNumber(number);
+    Console.WriteLine($"Сумма чисел от 1 до n: {product}");
+}
+else
+{
+    Console.WriteLine($"Некоретное число");
+}
+
+
