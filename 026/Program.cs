22ae65568eea7eb13caf5d7cc6cc206534cefe77
ruslan <rus.pro94@bk.ru>
2022-05-23 12:17:36 +0500

    026

---
+++ 026/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 26: Напишите программу, которая принимает
+ на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+
+string GetNumber ()
+{
+    return Console.ReadLine();
+}
+
+int CountNum(string str)
+{
+    return str.Length;
+}
+
+Console.WriteLine("Пожалуйста введите число:  ");
+int countNum = CountNum(GetNumber());
+Console.WriteLine(countNum);
+
